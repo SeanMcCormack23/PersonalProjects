ae5d06bc6f54f140de31ff26437047c9a6d9ca81
Sean McCormack <73062492+SeanMcCormack23@users.noreply.github.com>
2022-02-06 19:46:51 +0000

    expanding on the countdown timer app

---
+++ customTimer/script.js
@@ -0,0 +1,100 @@
+const dayNumber = document.getElementById("days");
+const hoursNumber = document.getElementById("hours");
+const minutesNumber = document.getElementById("minutes");
+const secondsNumber = document.getElementById("seconds");
+const heading = document.getElementById("heading");
+
+const ourDate = document.getElementById("theDate");
+
+ourDate.addEventListener("change", function () {
+  var input = "";
+  input = this.value;
+  var dateEntered = new Date(input);
+  console.log(input); //e.g. 2015-11-13
+  console.log(dateEntered); //e.g. Fri Nov 13 2015 00:00:00 GMT+0000 (GMT Standard Time)
+
+  // console.log(ourDate.value);
+  function countdown() {
+    // const inputtedDate = ourDate.value;
+    const today = new Date();
+
+    const totalSeconds = (dateEntered - today) / 1000;
+
+    // console.log(totalSeconds);
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    dayNumber.innerHTML = days;
+    hoursNumber.innerHTML = formatTime(hours);
+    minutesNumber.innerHTML = formatTime(mins);
+    secondsNumber.innerText = formatTime(seconds);
+
+  }
+// countdown();
+// countInterval = setInterval(countdown, 1000);
+//   countInterval;
+var x = 0;
+const countInterval = setInterval(function() {
+  countdown();
+  if (++x == 30){
+    window.clearInterval(countInterval);
+  }
+}, 1000);
+});
+
+
+
+//ternary operation to append a 0 before the result if less than 10 
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+const form = document.getElementById("userInfo");
+
+//add an event listener listening for SUBMIT to prevent the form data clearing on submit 
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // console.log(fname.value + " " +lname.value + " " + reason.value);
+
+  extraInfo();
+  theOccasion();
+  
+
+ 
+
+})
+
+function extraInfo() {
+  const name = fname.value + " " + lname.value;
+  const reasoning = reason.value;
+
+  function personalise() {
+    heading.innerHTML = "It is " + name + "'s " + reasoning + " in...";
+  }
+  personalise();
+}
+
+function theOccasion() {
+  if (reason.value == "birthday") {
+    document.body.style.backgroundImage = "url('bday.jpg')";
+  } else if (reason.value == "anniversary") {
+    document.body.style.backgroundImage = "url('anniversary.jpg')";
+  } else if (reason.value == "new year") {
+    document.body.style.backgroundImage = "url('newyear.jpg')";
+  } else if (reason.value == "holiday") {
+    document.body.style.backgroundImage = "url('holiday.jpg')";
+  } else if (reason.value == "operation"){
+    document.body.style.backgroundImage = "url('op.jpg')";
+  } else if (reason.value == "deadline") {
+    document.body.style.backgroundImage = "url('deadline.jpg')";
+  }
+}
+
+
+
+
