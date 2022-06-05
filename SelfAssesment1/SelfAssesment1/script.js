733f1352f9c7f78fe46aa252af204518dfb92195
Sean McCormack <73062492+SeanMcCormack23@users.noreply.github.com>
2022-06-05 19:57:29 +0100

    Finished off form and added styling
    included some images using flexbox and altered sizes based on device been viewed on
    all images either mine or from free sources

---
+++ SelfAssesment1/SelfAssesment1/script.js
@@ -0,0 +1,9 @@
+//small function to change radio button automatically if user selects "OTHER" in preffered activity
+
+var otherText = document.getElementById("otherTextbox");
+var otherRadio = document.getElementById("other");
+
+otherText.addEventListener("click", function() {
+  otherRadio.checked = true;
+});
+
